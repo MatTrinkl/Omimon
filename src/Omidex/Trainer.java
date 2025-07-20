0980a218e6638f03688bb64117a237338ead775d
matthiastrinkl <matthias.trinkl@gmail.com>
2025-07-21 00:31:22 +0200

    Start Edit Trainer

--- src/Omidex/Trainer.java
+++ src/Omidex/Trainer.java
@@ -1,5 +1,26 @@
 package Omidex;
 
+import Omidex.Omimon.Omimon;
+import Omidex.Omimon.OmimonBlueprint;
+import java.util.List;
+
 public class Trainer {
+
+  private List<Omimon> omimons;
+  private String name;
+  private String gender;
+  private int id;
+
+  private Strategy actionStrategy;
+  private Strategy battleStrategy;
+
+  public boolean CaptureOmmimon(OmimonBlueprint blueprint, String name, int level) {
+    if (omimons.size() < 6) {
+      blueprint.createInstance(name, level);
+      return true;
+    } else {
+      throw new ArrayIndexOutOfBoundsException("There can be only 6 Omimons");
+    }
+  }
 
 }
